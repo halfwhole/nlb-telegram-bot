1276fe933036c8ec349045b4e07c2049eb96f0e7
halfwhole <limziwei2@gmail.com>
2020-06-07 17:32:29 +0800

    Add 'add' conversation handler

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from config import token
 
 print(token)
@@ -9,16 +9,42 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+## States
+ADD_CONTINUE = range(1)
 
 ## Command handlers
 def start(update, context):
     update.message.reply_text('Hi!')
 
 def help(update, context):
-    update.message.reply_text('Help!')
+    help_message = """
+    You can do the following:
+- /start
+- /help
+- /add
+    """
+    update.message.reply_text(help_message)
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+def add_start(update, context):
+    update.message.reply_text('What book would you like to add next? Use /end to end at any time.')
+    return ADD_CONTINUE
+
+def add_continue(update, context):
+    text = update.message.text.strip()
+    try:
+        book_id = int(text)
+        print('Add book with ID %d' % book_id)
+        update.message.reply_text('Added book with id %d. What book would you like to add next? Use /end to end at any time.' % book_id)
+    except ValueError:
+        update.message.reply_text('That was not a valid book ID! Please try again.')
+    return ADD_CONTINUE
+
+def add_end(update, context):
+    update.message.reply_text('Books added, exiting.')
+    return ConversationHandler.END
+
+def four_oh_four(update, context):
+    update.message.reply_text("I didn't understand your message '%s'!" % update.message.text)
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -28,10 +54,20 @@ def main():
     updater = Updater(token, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
 
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    add_book_handler = ConversationHandler(
+        entry_points = [CommandHandler('add', add_start)],
+        states = {
+            ADD_CONTINUE: [CommandHandler('end', add_end),
+                           MessageHandler(Filters.text, add_continue)]
+        },
+        fallbacks = [CommandHandler('end', add_end)]
+    )
+    dp.add_handler(add_book_handler)
+
+    dp.add_handler(MessageHandler(Filters.text, four_oh_four))
 
     dp.add_error_handler(error)
 
