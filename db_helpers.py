9b8809fd9d5f0e8c27fd5d022911f35eebef35f3
halfwhole <limziwei2@gmail.com>
2020-06-11 18:01:55 +0800

    Refactor db_helper private methods into models

--- db_helpers.py
+++ db_helpers.py
@@ -1,113 +1,39 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from functools import lru_cache
-import time
-
-from config import conn_string
 from models import Book, Availability
 
-## TODO: Refactor into models.py and STANDARDISE NAMING (_db suffix)
+## TODO: STANDARDISE NAMING (_db suffix)
 
 def is_book_present(bid, user_id):
-    return _get_book(bid, user_id) is not None
+    return Book.get(bid, user_id) is not None
 
 def get_book_title_details(bid, user_id):
-    book = _get_book(bid, user_id)
+    book = Book.get(bid, user_id)
     return { 'title': book.title, 'author': book.author }
 
 def add_book_availabilities_db(bid, user_id, title_details, availability_info):
-    if _get_book(bid, user_id):
+    if Book.get(bid, user_id):
         raise ValueError('Book with bid=%d and user_id=%d already exists' % (bid, user_id))
     title = title_details['title']
     author = title_details['author']
-    book_id = _create_book(bid, user_id, title, author)
+    book_id = Book.create(bid, user_id, title, author)
     refresh_availabilities_db(book_id, availability_info)
 
 def refresh_availabilities_db(book_id, availability_info):
-    _delete_availabilities(book_id)
+    Availability.delete_all(book_id)
     for availability in availability_info:
         branch_name = availability['branch_name']
         call_number = availability['call_number']
         status_desc = availability['status_desc']
         shelf_location = availability['shelf_location']
-        _create_availability(book_id, branch_name, call_number, status_desc, shelf_location)
+        Availability.create(book_id, branch_name, call_number, status_desc, shelf_location)
 
 ## Deletes a Book and its Availabilities, returning the deleted book's ID
 def delete_book_and_availabilities(bid, user_id):
-    if _get_book(bid, user_id) is None:
+    if Book.get(bid, user_id) is None:
         raise LookupError('Book with bid=%d and user_id=%d does not exist' % (bid, user_id))
-    book_id = _delete_book_cascade(bid, user_id)
+    book_id = Book.delete_cascade(bid, user_id)
     return book_id
 
 ## TODO: Temporary solution to list book information, just gives bids, titles, and authors for now. Modify as necessary
 def get_book_info(user_id):
-    books = _get_books_by_user_id(user_id)
+    books = Book.get_all(user_id)
     return [{ 'bid': book.bid, 'title': book.title, 'author': book.author } for book in books]
-
-#####################
-## PRIVATE METHODS ##
-#####################
-
-## Remember to call session.close() after you finish!
-def _create_session():
-    engine = create_engine(conn_string)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
-
-## Creates a Book, returning its ID
-def _create_book(bid, user_id, title, author):
-    session = _create_session()
-    book = Book(bid=bid, user_id=user_id, title=title, author=author)
-    session.add(book)
-    session.commit()
-    book_id = book.id
-    session.close()
-    return book_id
-
-def _delete_book_cascade(bid, user_id):
-    session = _create_session()
-    book = _get_book(bid, user_id)
-    book_id = book.id
-    _delete_availabilities(book_id)
-    session.delete(book)
-    session.commit()
-    session.close()
-    return book_id
-
-def _get_book(bid, user_id):
-    session = _create_session()
-    book = session.query(Book).filter(Book.bid == bid).filter(Book.user_id == user_id).first()
-    session.close()
-    return book
-
-def _get_books_by_user_id(user_id):
-    session = _create_session()
-    books = session.query(Book).filter(Book.user_id == user_id).all()
-    session.close()
-    return books
-
-## Creates an Availability, returning its ID
-def _create_availability(book_id, branch_name, call_number, status_desc, shelf_location):
-    session = _create_session()
-    availability = Availability(
-        book_id=book_id,
-        branch_name=branch_name,
-        call_number=call_number,
-        status_desc=status_desc,
-        shelf_location=shelf_location
-    )
-    session.add(availability)
-    session.commit()
-    availability_id = availability.id
-    session.close()
-    return availability_id
-
-## Deletes all Availabilities referencing the book_id
-def _delete_availabilities(book_id):
-    session = _create_session()
-    availabilities = session.query(Availability).filter(Availability.book_id == book_id).all()
-    for availability in availabilities:
-        session.delete(availability)
-    session.commit()
-    session.close()
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+from config import conn_string
 
 Base = declarative_base()
 
@@ -19,9 +21,46 @@ class Book(Base):
     author = Column(String(200))
     availabilities = relationship('Availability', backref='book')
 
+    @staticmethod
+    def get(bid, user_id):
+        session = _create_session()
+        book = session.query(Book).filter(Book.bid == bid).filter(Book.user_id == user_id).first()
+        session.close()
+        return book
+
+    # Gets all books with the given user_id
+    @staticmethod
+    def get_all(user_id):
+        session = _create_session()
+        books = session.query(Book).filter(Book.user_id == user_id).all()
+        session.close()
+        return books
+
+    @staticmethod
+    def create(bid, user_id, title, author):
+        session = _create_session()
+        book = Book(bid=bid, user_id=user_id, title=title, author=author)
+        session.add(book)
+        session.commit()
+        book_id = book.id
+        session.close()
+        return book_id
+
+    @staticmethod
+    def delete_cascade(bid, user_id):
+        session = _create_session()
+        book = Book.get(bid, user_id)
+        book_id = book.id
+        Availability.delete_all(book_id)
+        session.delete(book)
+        session.commit()
+        session.close()
+        return book_id
+
     def __repr__(self):
         return "<Book(id=%s, bid=%d, user_id=%d, title='%s', author='%s')" % (
             self.id, self.bid, self.user_id, self.title, self.author)
+
 
 class Availability(Base):
     __tablename__ = 'availability'
@@ -33,14 +72,44 @@ class Availability(Base):
     status_desc = Column(String(200))
     shelf_location = Column(String(200))
 
+    @staticmethod
+    def create(book_id, branch_name, call_number, status_desc, shelf_location):
+        session = _create_session()
+        availability = Availability(
+            book_id=book_id,
+            branch_name=branch_name,
+            call_number=call_number,
+            status_desc=status_desc,
+            shelf_location=shelf_location
+        )
+        session.add(availability)
+        session.commit()
+        availability_id = availability.id
+        session.close()
+        return availability_id
+
+    ## Deletes all Availabilities referencing the given book id
+    @staticmethod
+    def delete_all(book_id):
+        session = _create_session()
+        availabilities = session.query(Availability).filter(Availability.book_id == book_id).all()
+        for availability in availabilities:
+            session.delete(availability)
+        session.commit()
+        session.close()
+
     def __repr__(self):
         return "<Availability(id=%s, book_id=%d, branch_name='%s', status_desc='%s')" % (
             self.id, self.book_id, self.branch_name, self.status_desc)
 
-if __name__ == '__main__':
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from config import conn_string
+
+## Remember to call session.close() after you finish!
+def _create_session():
     engine = create_engine(conn_string)
     Session = sessionmaker(bind=engine)
     session = Session()
+    return session
+
+
+if __name__ == '__main__':
+    session = _create_session()
