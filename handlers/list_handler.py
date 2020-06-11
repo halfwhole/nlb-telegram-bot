92333c22d0f9115c47a41c61135dccfb63ec7c4c
halfwhole <limziwei2@gmail.com>
2020-06-11 17:40:50 +0800

    Fix empty list book info bug

--- handlers/list_handler.py
+++ handlers/list_handler.py
@@ -1,17 +1,19 @@
+import telegram
 from telegram.ext import CommandHandler
 
 from db_helpers import get_book_info
 
-NO_BOOKS_STRING = 'You have no books! Use /add to start adding some.'
+BOOKS_PREFIX = '*Books*\n'
+NO_BOOKS_STRING = 'You have no books!\nUse /add to start adding new books.'
 
 def lst(update, context):
     user_id = int(update.message.from_user['id'])
     book_info = get_book_info(user_id)
-    text = '\n'.join('%d: %s' % (bi['bid'], bi['title']) for bi in book_info)
-    if not text:
+    if not book_info:
         update.message.reply_text(NO_BOOKS_STRING)
         return
 
-    update.message.reply_text(text)
+    text = BOOKS_PREFIX + '\n'.join('• %d: %s' % (bi['bid'], bi['title']) for bi in book_info)
+    update.message.reply_text(text, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 list_handler = CommandHandler('list', lst)
