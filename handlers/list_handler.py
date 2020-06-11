0ff9e3f661ece599848eea7e08036f931e5ac1b8
halfwhole <limziwei2@gmail.com>
2020-06-11 17:28:17 +0800

    Allow list to handle no books

--- handlers/list_handler.py
+++ handlers/list_handler.py
@@ -2,10 +2,16 @@ from telegram.ext import CommandHandler
 
 from db_helpers import get_book_info
 
+NO_BOOKS_STRING = 'You have no books! Use /add to start adding some.'
+
 def lst(update, context):
     user_id = int(update.message.from_user['id'])
     book_info = get_book_info(user_id)
     text = '\n'.join('%d: %s' % (bi['bid'], bi['title']) for bi in book_info)
+    if not text:
+        update.message.reply_text(NO_BOOKS_STRING)
+        return
+
     update.message.reply_text(text)
 
 list_handler = CommandHandler('list', lst)
